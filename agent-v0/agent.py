d436d9296a9b7a18842d3aec7d108222c6ed35f4
Calen Robinson <lokera6@gmail.com>
2022-03-07 09:50:12 -0800

    Create skeleton agent and main code

---
+++ agent-v0/agent.py
@@ -0,0 +1,9 @@
+class Agent:
+    def __init__(self):
+        pass
+
+    def choose_action(self, game_state):
+        pass
+
+    def __call__(self, x):
+        return choose_action(x)
---
+++ agent-v0/play_game.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+import gym
+import gym_mad
+
+def main(args):
+    env = gym.make("mad-v0")
+
+
+if __name__ == "__main__":
+    main(None)
