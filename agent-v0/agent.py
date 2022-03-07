87cc28f998b84f4b022f49ff65ebecadc69510fa
Calen Robinson <lokera6@gmail.com>
2022-03-07 12:37:20 -0800

    Implement random agent, play game

--- agent-v0/agent.py
+++ agent-v0/agent.py
@@ -1,9 +1,23 @@
-class Agent:
-    def __init__(self):
-        pass
+import numpy as np
 
-    def choose_action(self, game_state):
+
+class MadAgent_v0:
+    def __init__(self, observation_size, action_size):
+        self.observation_size = observation_size
+        self.action_size = action_size
+
+    def choose_action(self, observation):
+        return self.choose_random_action()
+
+    def choose_random_action(self):
+        action_vec = np.zeros((5))
+        action_idx = np.random.randint(0, 5)
+        action_vec[action_idx] = 1
+        return action_vec
+
+    def report_SARS(self, observation, action, reward, new_observation):
+        # TODO
         pass
 
     def __call__(self, x):
-        return choose_action(x)
+        return self.choose_action(x)
--- agent-v0/play_game.py
+++ agent-v0/play_game.py
@@ -2,10 +2,50 @@
 
 import gym
 import gym_mad
+import numpy as np
+from agent import MadAgent_v0
+import time
+
 
 def main(args):
+
     env = gym.make("mad-v0")
+    observations = env.reset()
+    done = False
+
+    # Currently setup for two AI playing against each other
+    # Seperate AI for each player
+    agent_a = MadAgent_v0(env.observation_size, env.action_size)
+    agent_b = MadAgent_v0(env.observation_size, env.action_size)
+
+    while not done:
+
+        print(f"Begin {env.current_player}'s turn")
+        if env.current_player == env.agent_a:
+            action_vec = agent_a(observations[env.agent_a])
+            new_observations, reward, done, info = env.step(action_vec)
+            print(f"Player took action {info['action'].action_str}")
+            print("New State:")
+            env.render()
+            agent_a.report_SARS(
+                observations[env.agent_a],
+                action_vec, reward, new_observations[env.agent_a])
+        else:
+            action_vec = agent_b(observations[env.agent_b])
+            new_observations, reward, done, info = env.step(action_vec)
+            print(f"Player took action {info['action'].action_str}")
+            print("New State:")
+            env.render()
+            agent_b.report_SARS(
+                observations[env.agent_b],
+                action_vec, reward, new_observations[env.agent_b])
+
+        time.sleep(3)
+        observations = new_observations
+
+    print("Game Over!")
 
 
 if __name__ == "__main__":
+    # TODO: Do we need commandline args?
     main(None)
